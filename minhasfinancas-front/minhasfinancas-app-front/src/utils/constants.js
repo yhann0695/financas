273e497cb3304a09450aa7e2d0061b778d5828da
yhann0695 <yhann0695@hotmail.com>
2021-07-06 22:08:46 -0300

    Criação de classe Constants para organizar mensagens

---
+++ minhasfinancas-front/minhasfinancas-app-front/src/utils/constants.js
@@ -0,0 +1,7 @@
+export default class Constants {
+    static NOME_OBRIGATORIO = 'O nome é obrigatório.';
+    static EMAIL_OBRIGATORIO = 'O campo email é obrigatório.'
+    static EMAIL_INVALIDO = 'Informe um email válido.';
+    static SENHA_REPETIR = 'Digite a senha duas vezes.';
+    static SENHA_DIFERENTE = 'As senhas precisam ser iguais.';
+}
